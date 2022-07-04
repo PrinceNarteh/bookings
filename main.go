2b1e643db87d4a96044ef5eaf41c8bd0da7734b2
Prince Narteh <prinarteh@gmail.com>
2022-07-04 16:07:24 +0000

    functions and handlers

--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+const portNumber = ":8080"
 
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Home Page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the About Page")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("🚀 Server running on http://localhost%s\n", portNumber)
 	http.ListenAndServe(":8080", nil)
 }
