d71d91202a4b73f207917acb10e00f64b4b7f7eb
Prince Narteh <prinarteh@gmail.com>
2022-07-05 12:50:34 +0000

    Serving HTML Templates

--- main.go
+++ main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home Page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the About Page")
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template")
+		return
+	}
 }
 
 func main() {
