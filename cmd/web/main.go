66a154ad8b3ac224d86ecbd05a0550a461803817
Prince Narteh <prinarteh@gmail.com>
2022-07-08 13:45:43 +0000

    adds handlers repository

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,16 +14,18 @@ const portNumber = ":8080"
 func main() {
 	var app config.AppConfig
 
-	ts, err := render.CreateTemplateCache()
+	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	app.TemplateCache = ts
+	app.TemplateCache = tc
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.Home)
 
 	fmt.Printf("🚀 Server running on http://localhost%s\n", portNumber)
 	log.Panicln(http.ListenAndServe(":8080", nil))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,37 @@
 package handlers
 
 import (
+	"hotel_reservation/pkg/config"
 	"hotel_reservation/pkg/render"
 	"net/http"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+//NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (re *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, "home.page.tmpl")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (re *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, "about.page.tmpl")
 }
